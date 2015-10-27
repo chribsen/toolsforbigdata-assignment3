97c5d520ffdf0a00d9e803d9af36a3af6888eb1c
Christian Danielsen <Christian@Christians-MacBook-Pro-2.local>
2015-10-27 14:27:11 +0100

    Made exercise 8.1

---
+++ tests/test_mapreduce_word_count.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+import nltk
+from nltk.tokenize import RegexpTokenizer
+
+r = RegexpTokenizer(u'\w+|[^\w\s\ufeff]+')
+
+class MRWordCount(MRJob):
+    def mapper(self, key, line):
+        for word in r.tokenize(line):
+            yield word.lower(), 1
+
+    def combiner(self, word, c):
+        yield word, sum(c)
+
+    def reducer(self, key, vals):
+        yield key, sum(vals)
+
+if __name__ == '__main__':
+    MRWordCount.run()
